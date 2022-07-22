13a3b2fbd560a1b14eeb9c65736e5a932a8caccf
SamSapuel <donshevan@yandex.ru>
2022-07-23 00:21:52 +0200

    test user controller

---
+++ user/src/main/java/com/users/application/UserRepository.java
@@ -0,0 +1,9 @@
+package com.users.application;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findUserByEmail(String email);
+}
