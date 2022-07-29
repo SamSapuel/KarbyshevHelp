5ce5771e779156db527d1fa32be9344449cbf9ca
Andrey Shevtsov <dev-shevtsov@pm.me>
2022-07-29 20:51:23 +0600

    refactor service class

--- task/src/main/java/com/tasks/application/components/TaskService.java
+++ task/src/main/java/com/tasks/application/components/TaskService.java
@@ -43,13 +43,37 @@ public class TaskService {
          }
      }
 
-    private void updateTask(List<String> props) {
+    private String updateTask(List<String> props) {
         // props structure is update-task:taskName:assignedTo
         // TODO: Implement user validation
+        Optional<Task> checkedTask = taskRepository.findByLabel(props.get(0));
+        if (checkedTask.isEmpty()) {
+            System.err.println(String.format("task %s does not exists", props.get(0)));
+            return "update-task:404";
+        } else {
+            // checking if user is valid
+
+            // TODO: implement assigned user validation
+
+            // after validation
+            Task replacedTask = checkedTask.get();
+            replacedTask.setAssignedTo(props.get(1));
+            // deleting old task
+            try {
+                taskRepository.delete(checkedTask.get());
+            } catch (Exception e) {
+                throw new RuntimeException("failed to delete task" + props.get(0));
+            }
+            taskRepository.save(replacedTask);
+            return "update-task:OK";
+        }
     }
 
-    private void completeTask(Task task) {
+    private void completeTask(List<String> props) {
         // TODO: perform checking if task doesn't exists or already completed
-        task.setCompleted(true);
+        Optional<Task> checkedTask = taskRepository.findByLabel(props.get(0));
+        if (checkedTask.isEmpty()) {
+
+        }
     }
 }
